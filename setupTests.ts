2752a4324735d70d845eaf3c91b7cf8da98e2afa
Michael Päßler <kolorfilm@gmail.com>
2023-09-06 09:49:14 +0200

    refactor setupTests.js

--- setupTests.ts
+++ setupTests.ts
@@ -1,17 +1,15 @@
-export {}
+import fetchMock from 'jest-fetch-mock'
+import { TextEncoder } from 'util'
+
+fetchMock.enableMocks()
+jest.mock('next/router', () => require('next-router-mock'))
 
 process.env.DISCOGS_APP_URL = 'http://localhost:3000'
 process.env.DISCOGS_CALLBACK_URL = 'http://localhost:3000/callback'
-
-require('jest-fetch-mock').enableMocks()
-
-jest.mock('next/router', () => require('next-router-mock'))
 
 Object.defineProperty(window.document, 'cookie', {
   writable: true,
   value: '',
 })
-
-import { TextEncoder } from 'util'
 
 global.TextEncoder = TextEncoder

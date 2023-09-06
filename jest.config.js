338a3d5cb322c2396631a8f7f5d2a66474166e66
Michael Päßler <kolorfilm@gmail.com>
2023-09-06 09:48:26 +0200

    - remove package @testing-library/dom that is not necessary anymore - add @types/jest package - move out jest config from package.json to jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFiles: ['./setupTests.ts'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom'],
+  testPathIgnorePatterns: ['/node_modules/'],
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!<rootDir>/next-env.d.ts',
+    '!<rootDir>/next.config.js',
+    '!.next/**',
+    '!**/node_modules/**',
+    '!**/libs/**',
+  ],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  transformIgnorePatterns: [',<rootDir>/node_modules/'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': [
+      'babel-jest',
+      {
+        presets: ['next/babel'],
+        plugins: ['@babel/plugin-proposal-private-methods'],
+      },
+    ],
+  },
+}

deab3e1ba1963c0579e8a40f24778e7eaa4fa144
Michael Päßler <kolorfilm@gmail.com>
2026-07-03 16:11:14 +0200

    Migrate cookie helpers to cookie v2 API.
    Replace deprecated parse/serialize with parseCookie and stringifySetCookie.

--- server/utils/getAccessDataCookie.ts
+++ server/utils/getAccessDataCookie.ts
@@ -1,11 +1,11 @@
 import { Request } from 'express';
-import { parse } from 'cookie';
+import { parseCookie } from 'cookie';
 import { RequestData } from '../../types/RequestData';
 import { ACCESS_DATA_COOKIE_NAME } from './constants';
 
 export default function getAccessDataCookie(req: Request): RequestData | null {
   const cookieHeader = req.headers.cookie || '';
-  const cookies = parse(cookieHeader);
+  const cookies = parseCookie(cookieHeader);
   const accessDataCookie = cookies?.[ACCESS_DATA_COOKIE_NAME] ?? null;
 
   return accessDataCookie ? JSON.parse(accessDataCookie) : null;
